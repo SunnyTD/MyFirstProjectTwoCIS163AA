bdb48b1e503bec79649ceaf1010440d8e2543f45
Daniel <djplotzke@gmail.com>
2017-10-12 02:35:13 -0700

    Initial commit

---
+++ src/myfirstprojectmk/ii/MyFirstProjectMkII.java
@@ -0,0 +1,22 @@
+/*
+ Daniel Plotzke
+9/14/17
+CIS163AA #26008
+Week 2
+Prints out my full name, "I got it working", and "WOOOOOO!".
+ */
+package myfirstprojectmk.ii;
+
+
+public class MyFirstProjectMkII 
+{
+
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Daniel Plotzke");
+        System.out.println("I got it working!");
+        System.out.println("WOOOOOO!");
+    }
+    
+}
